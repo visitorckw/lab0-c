832af5adecc8e017db34f949f97000e76e1f0dc6
visitorckw <visitorckw@gmail.com>
2023-02-19 22:03:28 +0800

    Add q_free function
    Use list_for_each_entry_safe macro to iterate through each
    element, then free value and the whole element in order.

--- queue.c
+++ queue.c
@@ -22,7 +22,16 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    element_t *entry;
+    element_t *safe;
+    list_for_each_entry_safe (entry, safe, l, list) {
+        free(entry->value);
+        free(entry);
+    }
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
