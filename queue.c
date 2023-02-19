44dfb578b904ffffce988d72d8667a4f5b6a542e
visitorckw <visitorckw@gmail.com>
2023-02-19 22:37:33 +0800

    Add q_remove_head and q_remove_tail function
    Remove a node from head or tail of the queue.
    Use list_entry macro to obtain the entry to remove.
    Then copy the string of the removed element to sp.
    Finally use list_del macro to remove the node from queue.

--- queue.c
+++ queue.c
@@ -80,13 +80,29 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *delNode = list_entry(head->next, element_t, list);
+    size_t length = bufsize < strlen(delNode->value) + 1
+                        ? bufsize
+                        : strlen(delNode->value) + 1;
+    strlcpy(sp, delNode->value, length);
+    list_del(head->next);
+    return delNode;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *delNode = list_entry(head->prev, element_t, list);
+    size_t length = bufsize < strlen(delNode->value) + 1
+                        ? bufsize
+                        : strlen(delNode->value) + 1;
+    strlcpy(sp, delNode->value, length);
+    list_del(head->prev);
+    return delNode;
 }
 
 /* Return number of elements in queue */
